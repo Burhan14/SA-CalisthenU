abb69d4ed9d9733277f6cce5eafcf6197bd47850
Fuat Yöndem <fuat.yondem@student.odisee.be>
2021-10-24 16:44:39 +0200

    .

--- CalisthenU/src/app/locations/locations.component.ts
+++ CalisthenU/src/app/locations/locations.component.ts
@@ -9,18 +9,25 @@ export interface Locations {
 
 @Component({
   selector: 'app-locations',
-  templateUrl: './locations.component.html',
+  // templateUrl: './locations.component.html',
+  template: `<mat-list role="list">
+  <p *ngFor="let loc of locations">
+    <mat-list-item role="listitem">{{loc.name}}</mat-list-item>
+    <mat-divider></mat-divider>
+  </p>
+  
+</mat-list>`,
   styleUrls: ['./locations.component.css']
 })
 export class LocationsComponent {
-  // locations: Observable<Locations>;
-  // constructor(private db: AngularFirestore) {
-  //   this.locations = db.collection('locations').valueChanges();
-  //   this.locations.subscribe(console.table);
+  locations: any;
+  constructor(private db: AngularFirestore) {
+    this.locations = db.collection('locations').valueChanges();
+    this.locations.subscribe(console.table);
 
   // ngOnInit(): void {
   // }
 
   // }
 }
-
+}
