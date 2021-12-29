3921198e4e65a03743fece18fc49cce8f01d68d9
Burhan <burhan.atesalp@student.odisee.be>
2021-12-29 20:07:45 +0100

    Reviews

--- CalisthenU/src/app/components/review/review.component.ts
+++ CalisthenU/src/app/components/review/review.component.ts
@@ -1,6 +1,6 @@
 import { ReviewService } from './../../shared/services/review/review.service';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-review',
@@ -8,11 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./review.component.css']
 })
 export class ReviewComponent implements OnInit {
+  @Input() locId:string;
+
 
   constructor(private afStorage:AngularFireStorage, private reviewService: ReviewService ) { }
   currentRating: number = 1;
   data:any = new Object();
+
+  review:any
+  createdBy: string;
+  comment: string;
+  score:number;
+  date:any;
+
   ngOnInit(): void {
+    this.GetReviewsById();
   }
 
   public sendReview(comment: string) {
@@ -25,6 +35,7 @@ export class ReviewComponent implements OnInit {
         // console.log(comment + " " + this.currentRating);
         this.data.comment = comment;
       }
+      this.data.locId = this.locId;
       this.reviewService.CreateReview(this.data)
       // console.log(this.data);
 
@@ -33,4 +44,32 @@ export class ReviewComponent implements OnInit {
   public UpdateRating(rating: number) {
     this.currentRating = rating;
   }
+
+  //array of reviews, filled in directly on init from db
+  reviews: any = [];
+
+  //call service to fetch data from db and push into reviews array
+  // GetReviews = () =>
+  //   this.reviewService
+  //   .GetReviews()
+  //   .subscribe(res => {
+  //     this.reviews = res;
+  //   });  
+
+  //call service to fetch data from db and push into reviews array
+    GetReviewsById = () =>
+    this.reviewService
+    .GetReviewById(this.locId)
+    .subscribe(res => {
+      this.review = res.payload.data();
+      console.log(this.review);
+      this.date = this.review.creationDate;
+      this.score = this.review.rating;
+      this.comment = this.review.comment;
+      this.createdBy = this.review.createdByDN;
+    });  
+
+    // werkt niet
+    // toon enkel de reviews waar de locid gelijk is aan de id van de huidige locatie.
+
 }
